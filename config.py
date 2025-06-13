8491b0097a0ba9ce2d74afa88e8061d1417051ec
ASHISHTIME <155893081+BWFTIME@users.noreply.github.com>
2025-06-13 21:54:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 # ━━━━━━━━━━━━━❖ ➥ 𝐿2𝙍 𝗠𝗨𝗦𝗜𝗖™🇮🇳━━━━━━━━━━━
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/BWFXMUSIC/L2RMUSICROBOT")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/BWFTIME/ASHISH")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)  # Fill this variable if your upstream repository is private
 
